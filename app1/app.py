29f7e4cb84f02a3c125f25e7ed6ad1b333614c92
xyziie <sharma.achyut17@gmail.com>
2025-04-19 14:46:04 +0000

    Updated code

--- app1/app.py
+++ app1/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World App 1!'
+    return 'Hello, Achyut. This is App 1!'
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
--- app2/app.py
+++ app2/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World app 2!'
+    return 'Hello, Achyut. This is Application 2!'
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
